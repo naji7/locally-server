6c5a59d9a0cc88daf65bc30eb3de6c6930abb5f4
cl-nasik <nasikn@codinglegends.io>
2024-05-10 15:50:23 +0530

    fixed bug

--- src/config/app.ts
+++ src/config/app.ts
@@ -4,7 +4,11 @@ export * from "./app";
 
 export const logDirPath = "../logs";
 
-const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://winland-api-b8e7f08fc1f1.herokuapp.com/",
+];
 
 export const corsOptions: CorsOptions = {
   origin: allowedOrigins,
